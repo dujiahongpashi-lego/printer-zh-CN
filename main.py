b48ae75aaea735bafb9f3a1af8d34a48c5abc461
sduworm <sdu.worm@hotmail.com>
2022-02-25 10:06:42 +0800

    收藏

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-""" 辅助程序 """
+"""
+辅助程序
+这个地址可以生成点阵字符：https://www.qqxiuzi.cn/zh/dianzhenzi-zifu/
+视频求赞求三连
+↑↑↑↑↑↑↑↑↑↑↑↑这句话你可能都看烦了吧？
+因为点赞三连什么的对视频真的很重要，所以我才这样百般讨要，抱歉抱歉，影响你的体验了。
+"""
 import os
 
 from ReadBMPFile import ReadBMPFile
@@ -7,7 +13,14 @@ filePaths = [
     'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/点-微软雅黑-18字号-25x25.bmp',
     'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/赞-微软雅黑-18字号-25x25.bmp',
     'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/三-微软雅黑-18字号-25x25.bmp',
-    'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/连-微软雅黑-18字号-25x25.bmp'
+    'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/连-微软雅黑-18字号-25x25.bmp',
+    'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/关-微软雅黑-18字号-25x25.bmp',
+    'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/注-微软雅黑-18字号-25x25.bmp',
+    'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/禁-微软雅黑-18字号-25x25.bmp',
+    'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/止-微软雅黑-18字号-25x25.bmp',
+    'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/吸-微软雅黑-18字号-25x25.bmp',
+    'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/烟-微软雅黑-18字号-25x25.bmp',
+    'C:/Users/sduwo/Desktop/019乐高打印机/25x25 BMP图像/smoke-icon-25x25.bmp',
 ]
 
 legoPrinterResolution = ['025', '025']
